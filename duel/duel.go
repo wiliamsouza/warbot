2394ec770436530dde6baa5e28e446a9a9ae5146
Wiliam Souza <wiliamsouza83@gmail.com>
2018-08-03 01:02:07 -0300

    Add registering mechanisms for duels and new helpers methods

--- duel/duel.go
+++ duel/duel.go
@@ -1,24 +1,92 @@
 package duel
 
-import "github.com/wiliamsouza/warbot/fighter"
+import (
+	"errors"
+	"sync"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/wiliamsouza/warbot/fighter"
+)
+
+var (
+	duelsMu sync.RWMutex
+	duels   = make(map[uuid.UUID]Dueler)
+)
 
 // Dueler interface
 type Dueler interface {
-	Duel(duelists ...fighter.Fighter) *DuelResult
+	Duel(duelists ...fighter.Fighter) *Result
+	SetOpponent(fighter fighter.Fighter)
+	GetFighter() fighter.Fighter
+	GetOpponent() fighter.Fighter
 }
 
 // Duel implements Dueler interface
-type Duel struct{}
+type Duel struct {
+	ID       uuid.UUID
+	Fighter  fighter.Fighter
+	Opponent fighter.Fighter
+}
 
-// DuelResult of the duel
-type DuelResult struct{}
+// Result of the duel
+type Result struct{}
 
 // Duel start the combat between two duelists
-func (d *Duel) Duel(duelists ...fighter.Fighter) *DuelResult {
-	return &DuelResult{}
+func (d *Duel) Duel(duelists ...fighter.Fighter) *Result {
+	return &Result{}
+}
+
+// SetOpponent a fighter that accepted the challenge
+func (d *Duel) SetOpponent(fighter fighter.Fighter) {
+	d.Opponent = fighter
+}
+
+// GetFighter return duel creator
+func (d *Duel) GetFighter() fighter.Fighter {
+	return d.Fighter
+}
+
+// GetOpponent return duel opponent
+func (d *Duel) GetOpponent() fighter.Fighter {
+	return d.Opponent
 }
 
 // NewDuel create duel
-func NewDuel() *Duel {
-	return &Duel{}
+func NewDuel(fighter fighter.Fighter) *Duel {
+	d := &Duel{ID: uuid.NewV4(), Fighter: fighter}
+	register(d.ID, d)
+	return d
+}
+
+// register a duel
+func register(id uuid.UUID, duel Dueler) {
+	duelsMu.Lock()
+	defer duelsMu.Unlock()
+	if duel == nil {
+		// TODO: Change to return error
+		panic("duels: Register duel is nil")
+	}
+	if _, dup := duels[id]; dup {
+		// TODO: Change to return error
+		panic("duel: Register called twice for duel ")
+	}
+	duels[id] = duel
+}
+
+// GetDuelID returns a duel by its ID
+func GetDuelID(id uuid.UUID) Dueler {
+	return duels[id]
+}
+
+// GetDuelByFighter returns a duel from the given fighter
+func GetDuelByFighter(f fighter.Fighter) (Dueler, error) {
+	for _, d := range duels {
+		if d.GetFighter().Identification() == f.Identification() {
+			return d, nil
+		}
+		if d.GetOpponent().Identification() == f.Identification() {
+			return d, nil
+		}
+	}
+	return &Duel{}, errors.New("Duel not found")
 }
