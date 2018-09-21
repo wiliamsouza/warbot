a6302aee9d73a8a16b4738c191929ad583e25de6
Wiliam Souza <wiliamsouza83@gmail.com>
2018-09-21 19:28:28 -0300

    Add bot commands

--- telegram/bot.go
+++ telegram/bot.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/wiliamsouza/warbot/duel"
@@ -22,13 +23,15 @@ func NewBot(token string) *tb.Bot {
 		log.Fatal(err)
 	}
 
-	arena := tb.ReplyButton{Text: "🗡 Arena"}
+	home := tb.ReplyButton{Text: "🏠 Home"}
+
+	arena := tb.ReplyButton{Text: "⚔ War"}
 	challenge := tb.ReplyButton{Text: "🤺 Challenge"}
 	battle := tb.ReplyButton{Text: "🗺 Battle"}
 
-	headAttack := tb.ReplyButton{Text: "🗡 Head"}
-	bodyAttack := tb.ReplyButton{Text: "🗡 Body"}
-	feetAttack := tb.ReplyButton{Text: "🗡 Feet"}
+	headAttack := tb.ReplyButton{Text: "⚔ Head"}
+	bodyAttack := tb.ReplyButton{Text: "⚔ Body"}
+	feetAttack := tb.ReplyButton{Text: "⚔ Feet"}
 
 	attackCommands := [][]tb.ReplyButton{
 		[]tb.ReplyButton{headAttack},
@@ -36,9 +39,9 @@ func NewBot(token string) *tb.Bot {
 		[]tb.ReplyButton{feetAttack},
 	}
 
-	headDefense := tb.ReplyButton{Text: "🛡 Head"}
-	bodyDefense := tb.ReplyButton{Text: "🛡 Body"}
-	feetDefense := tb.ReplyButton{Text: "🛡 Feet"}
+	headDefense := tb.ReplyButton{Text: "⛨ Head"}
+	bodyDefense := tb.ReplyButton{Text: "⛨ Body"}
+	feetDefense := tb.ReplyButton{Text: "⛨ Feet"}
 
 	defenseCommands := [][]tb.ReplyButton{
 		[]tb.ReplyButton{headDefense},
@@ -57,7 +60,7 @@ func NewBot(token string) *tb.Bot {
 
 	arenaCommands := [][]tb.ReplyButton{
 		[]tb.ReplyButton{challenge},
-		[]tb.ReplyButton{battle},
+		[]tb.ReplyButton{home},
 	}
 
 	commands := [][]tb.ReplyButton{
@@ -66,10 +69,10 @@ func NewBot(token string) *tb.Bot {
 
 	b.Handle(&challenge, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		f.SetType("challenger")
 		d := duel.NewDuel(f)
 
-		b.Send(m.Sender, "Your opponent has to forward this message to the @xwarsbot:")
+		b.Send(m.Sender, "Your opponent has to forward this message to the @twarsbot:")
+		// TODO: username is not available all time add a fallback first name
 		b.Send(m.Sender, f.GetUsername()+" challenges you to a duel!")
 		b.Send(m.Sender, "/duel "+d.ID.String())
 	})
@@ -86,76 +89,232 @@ func NewBot(token string) *tb.Bot {
 
 	b.Handle("/duel", func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		f.SetType("challenged")
 		u, err := uuid.FromString(m.Payload)
 		if err != nil {
 			b.Send(m.Sender, "Invalid duel identification!")
 		}
-		d := duel.GetDuelID(u)
-		d.SetOpponent(f)
+		// TODO: Add not found error return value here!
+		d := duel.GetDuelByID(u)
+		d.SetChallenged(f)
 		b.Send(m.Sender, "🤺 The duel has started!")
 		b.Send(m.Sender, "Choose point of attack and point of block.", &tb.ReplyMarkup{
 			ReplyKeyboard: attackDefenseCommands,
 		})
 
-		b.Send(d.GetFighter(), f.GetUsername()+" accepted your challenge!")
-		b.Send(d.GetFighter(), "🤺 The duel has started!")
-		b.Send(d.GetFighter(), "Choose point of attack and point of block.", &tb.ReplyMarkup{
+		// NOTE: Comunicate about challenger accepted to the challenger
+		// TODO: username is not available all time add a fallback first name
+		b.Send(d.GetChallenger(), f.GetUsername()+" accepted your challenge!")
+		b.Send(d.GetChallenger(), "🤺 The duel has started!")
+		b.Send(d.GetChallenger(), "Choose point of attack and point of block.", &tb.ReplyMarkup{
 			ReplyKeyboard: attackDefenseCommands,
 		})
 	})
 
 	b.Handle(&headAttack, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewAttack("head")
-		f.SetAttack(a)
-		b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
-			ReplyKeyboard: defenseCommands,
-		})
+		f.SetAttack("head")
+		if !f.HasDefense() {
+			b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
+				ReplyKeyboard: defenseCommands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+			d, _ := duel.GetDuelByFighter(f)
+			if d.Ready() {
+				result := d.Duel()
+				if d.Finished() {
+					w := d.Winner()
+					b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+					b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+				}
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
 	})
 
 	b.Handle(&bodyAttack, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewAttack("body")
-		f.SetAttack(a)
-		b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
-			ReplyKeyboard: defenseCommands,
-		})
+		f.SetAttack("body")
+
+		d, _ := duel.GetDuelByFighter(f)
+
+		if !f.HasDefense() {
+			b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
+				ReplyKeyboard: defenseCommands,
+			})
+		}
+		if d.Finished() {
+			w := d.Winner()
+			b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+				ReplyKeyboard: commands,
+			})
+			b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+				ReplyKeyboard: commands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+
+			if d.Ready() {
+				result := d.Duel()
+
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
 	})
 
 	b.Handle(&feetAttack, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewAttack("feet")
-		f.SetAttack(a)
-		b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
-			ReplyKeyboard: defenseCommands,
-		})
+		f.SetAttack("feet")
+		if !f.HasDefense() {
+			b.Send(m.Sender, "Great! Now select your defence!", &tb.ReplyMarkup{
+				ReplyKeyboard: defenseCommands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+			d, _ := duel.GetDuelByFighter(f)
+			if d.Ready() {
+				result := d.Duel()
+				if d.Finished() {
+					w := d.Winner()
+					b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+					b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+				}
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
 	})
 
 	b.Handle(&headDefense, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewDefense("head")
-		f.SetDefense(a)
-		b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
-			ReplyKeyboard: attackCommands,
-		})
+		f.SetDefense("head")
+		if !f.HasAttack() {
+			b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
+				ReplyKeyboard: attackCommands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+			d, _ := duel.GetDuelByFighter(f)
+			if d.Ready() {
+				result := d.Duel()
+				if d.Finished() {
+					w := d.Winner()
+					b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+					b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+				}
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
 	})
 
 	b.Handle(&bodyDefense, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewDefense("body")
-		f.SetDefense(a)
-		b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
-			ReplyKeyboard: attackCommands,
-		})
+		f.SetDefense("body")
+		if !f.HasAttack() {
+			b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
+				ReplyKeyboard: attackCommands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+			d, _ := duel.GetDuelByFighter(f)
+			if d.Ready() {
+				result := d.Duel()
+				if d.Finished() {
+					w := d.Winner()
+					b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+					b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+				}
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
 	})
 
 	b.Handle(&feetDefense, func(m *tb.Message) {
 		f := fighter.NewFighterFormTelegramUser(m.Sender)
-		a := fighter.NewDefense("feet")
-		f.SetDefense(a)
-		b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
-			ReplyKeyboard: attackCommands,
+		f.SetDefense("feet")
+		if !f.HasAttack() {
+			b.Send(m.Sender, "Great! Now select your attack!", &tb.ReplyMarkup{
+				ReplyKeyboard: attackCommands,
+			})
+		}
+		if f.Ready() {
+			b.Send(m.Sender, "Good plan! let's see what happens next.")
+			// TODO: Do not ignore this error!
+			d, _ := duel.GetDuelByFighter(f)
+			if d.Ready() {
+				result := d.Duel()
+				if d.Finished() {
+					w := d.Winner()
+					b.Send(d.GetChallenger(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+					b.Send(d.GetChallenged(), w.GetUsername()+"(❤ "+strconv.Itoa(w.Health())+") won the duel!", &tb.ReplyMarkup{
+						ReplyKeyboard: commands,
+					})
+				}
+				b.Send(d.GetChallenger(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+				b.Send(d.GetChallenged(), result.Challenged.Result+" "+result.Challenger.Result, &tb.ReplyMarkup{
+					ReplyKeyboard: attackDefenseCommands,
+				})
+			}
+		}
+	})
+
+	b.Handle(&home, func(m *tb.Message) {
+		b.Send(m.Sender, "🔙", &tb.ReplyMarkup{
+			ReplyKeyboard: commands,
 		})
 	})
 
